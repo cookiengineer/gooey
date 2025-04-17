dd07021dc1a69c35da5d349bc51300aa49309d86
Cookie Engineer <cookiengineer@protonmail.com>
2025-04-17 23:37:05 +0200

    :rocket: Table Example

---
+++ examples/components/content-table/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "github.com/cookiengineer/gooey/bindings"
+import "github.com/cookiengineer/gooey/bindings/console"
+import "github.com/cookiengineer/gooey/components"
+import "github.com/cookiengineer/gooey/components/content"
+import "time"
+
+func main() {
+
+	table := content.ToTable(bindings.Document.QuerySelector("table"))
+	table.Component.AddEventListener("change-select", components.ToEventListener(func(event string, attributes map[string]string) {
+
+		console.Group("table change-select event")
+		console.Log(attributes)
+		console.GroupEnd("table change-select event")
+
+	}, false))
+	table.Component.AddEventListener("change-sort", components.ToEventListener(func(event string, attributes map[string]string) {
+
+		console.Group("table change-sort event")
+		console.Log(attributes)
+		console.GroupEnd("table change-sort event")
+
+	}, false))
+
+	for true {
+
+		// Do Nothing
+		time.Sleep(1 * time.Second)
+
+	}
+
+}
