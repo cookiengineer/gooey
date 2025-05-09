15d87d757a9c389391c8caf83a88cd0a908454e7
Cookie Engineer <cookiengineer@protonmail.com>
2025-05-09 22:30:37 +0200

    :bug: Fixes for crypto Decrypt() methods

--- bindings/crypto/aesctr/Decrypt.go
+++ bindings/crypto/aesctr/Decrypt.go
@@ -10,73 +10,79 @@ type decrypt_state struct {
 
 func Decrypt(counter []byte, length uint, key *CryptoKey, buffer []byte) ([]byte, error) {
 
-	if len(counter) == 16 && length >= 64 && length <= 128 {
+	if key != nil && key.Value != nil {
 
-		channel := make(chan *decrypt_state)
+		if len(counter) == 16 && length >= 64 && length <= 128 {
 
-		algorithm := make(map[string]any)
-		wrapped_algorithm := js.ValueOf(algorithm)
-		wrapped_counter_array := js.Global().Get("Uint8Array").New(len(counter))
-		js.CopyBytesToJS(wrapped_counter_array, counter)
-		wrapped_algorithm.Set("name", "AES-CTR")
-		wrapped_algorithm.Set("counter", wrapped_counter_array)
-		wrapped_algorithm.Set("length", js.ValueOf(length))
+			channel := make(chan *decrypt_state)
 
-		wrapped_key := *key.Value
-		wrapped_buffer := js.Global().Get("Uint8Array").New(len(buffer))
+			algorithm := make(map[string]any)
+			wrapped_algorithm := js.ValueOf(algorithm)
+			wrapped_counter_array := js.Global().Get("Uint8Array").New(len(counter))
+			js.CopyBytesToJS(wrapped_counter_array, counter)
+			wrapped_algorithm.Set("name", "AES-CTR")
+			wrapped_algorithm.Set("counter", wrapped_counter_array)
+			wrapped_algorithm.Set("length", js.ValueOf(length))
 
-		js.CopyBytesToJS(wrapped_buffer, buffer)
+			wrapped_key := *key.Value
+			wrapped_buffer := js.Global().Get("Uint8Array").New(len(buffer))
 
-		on_success := js.FuncOf(func(this js.Value, args []js.Value) any {
+			js.CopyBytesToJS(wrapped_buffer, buffer)
 
-			array := js.Global().Get("Uint8Array").New(args[0])
-			buffer := make([]byte, array.Get("byteLength").Int())
-			js.CopyBytesToGo(buffer, array)
+			on_success := js.FuncOf(func(this js.Value, args []js.Value) any {
 
-			channel <- &decrypt_state{
-				buffer: buffer,
-				err:    nil,
+				array := js.Global().Get("Uint8Array").New(args[0])
+				buffer := make([]byte, array.Get("byteLength").Int())
+				js.CopyBytesToGo(buffer, array)
+
+				channel <- &decrypt_state{
+					buffer: buffer,
+					err:    nil,
+				}
+
+				return nil
+
+			})
+
+			defer on_success.Release()
+
+			on_failure := js.FuncOf(func(this js.Value, args []js.Value) any {
+
+				value := args[0]
+				message := value.Get("message").String()
+
+				channel <- &decrypt_state{
+					buffer: []byte{},
+					err:    errors.New(message),
+				}
+
+				return nil
+
+			})
+
+			defer on_failure.Release()
+
+			subtle := js.Global().Get("crypto").Get("subtle")
+
+			if subtle.IsNull() || subtle.IsUndefined() {
+				err := errors.New("Error: Unsecure WebPage Context.")
+				return []byte{}, err
 			}
 
-			return nil
+			go subtle.Call("decrypt", wrapped_algorithm, wrapped_key, wrapped_buffer).Call("then", on_success).Call("catch", on_failure)
 
-		})
+			state := <-channel
 
-		defer on_success.Release()
+			return state.buffer, state.err
 
-		on_failure := js.FuncOf(func(this js.Value, args []js.Value) any {
-
-			value := args[0]
-			message := value.Get("message").String()
-
-			channel <- &decrypt_state{
-				buffer: []byte{},
-				err:    errors.New(message),
-			}
-
-			return nil
-
-		})
-
-		defer on_failure.Release()
-
-		subtle := js.Global().Get("crypto").Get("subtle")
-
-		if subtle.IsNull() || subtle.IsUndefined() {
-			err := errors.New("Error: Unsecure WebPage Context.")
-			return []byte{}, err
+		} else if len(counter) != 16 {
+			return []byte{}, errors.New("AES-CTR expects counter to be 16 bytes long")
+		} else {
+			return []byte{}, errors.New("AES-CTR expects nonce/counter length to be between 64 and 128 bits")
 		}
 
-		go subtle.Call("decrypt", wrapped_algorithm, wrapped_key, wrapped_buffer).Call("then", on_success).Call("catch", on_failure)
-
-		state := <-channel
-
-		return state.buffer, state.err
-
-	} else if len(counter) != 16 {
-		return []byte{}, errors.New("AES-CTR expects counter to be 16 bytes long")
 	} else {
-		return []byte{}, errors.New("AES-CTR expects nonce/counter length to be between 64 and 128 bits")
+		return []byte{}, errors.New("Error: Invalid CryptoKey")
 	}
 
 }
--- bindings/crypto/aesgcm/Decrypt.go
+++ bindings/crypto/aesgcm/Decrypt.go
@@ -10,64 +10,70 @@ type decrypt_state struct {
 
 func Decrypt(iv []byte, key *CryptoKey, buffer []byte) ([]byte, error) {
 
-	channel := make(chan *decrypt_state)
+	if key != nil && key.Value != nil {
 
-	algorithm := make(map[string]any)
-	wrapped_algorithm := js.ValueOf(algorithm)
-	wrapped_iv_array := js.Global().Get("Uint8Array").New(len(iv))
-	js.CopyBytesToJS(wrapped_iv_array, iv)
-	wrapped_algorithm.Set("name", "AES-GCM")
-	wrapped_algorithm.Set("iv", wrapped_iv_array)
+		channel := make(chan *decrypt_state)
 
-	wrapped_key := *key.Value
-	wrapped_buffer := js.Global().Get("Uint8Array").New(len(buffer))
+		algorithm := make(map[string]any)
+		wrapped_algorithm := js.ValueOf(algorithm)
+		wrapped_iv_array := js.Global().Get("Uint8Array").New(len(iv))
+		js.CopyBytesToJS(wrapped_iv_array, iv)
+		wrapped_algorithm.Set("name", "AES-GCM")
+		wrapped_algorithm.Set("iv", wrapped_iv_array)
 
-	js.CopyBytesToJS(wrapped_buffer, buffer)
+		wrapped_key := *key.Value
+		wrapped_buffer := js.Global().Get("Uint8Array").New(len(buffer))
 
-	on_success := js.FuncOf(func(this js.Value, args []js.Value) any {
+		js.CopyBytesToJS(wrapped_buffer, buffer)
 
-		array := js.Global().Get("Uint8Array").New(args[0])
-		buffer := make([]byte, array.Get("byteLength").Int())
-		js.CopyBytesToGo(buffer, array)
+		on_success := js.FuncOf(func(this js.Value, args []js.Value) any {
 
-		channel <- &decrypt_state{
-			buffer: buffer,
-			err:    nil,
+			array := js.Global().Get("Uint8Array").New(args[0])
+			buffer := make([]byte, array.Get("byteLength").Int())
+			js.CopyBytesToGo(buffer, array)
+
+			channel <- &decrypt_state{
+				buffer: buffer,
+				err:    nil,
+			}
+
+			return nil
+
+		})
+
+		defer on_success.Release()
+
+		on_failure := js.FuncOf(func(this js.Value, args []js.Value) any {
+
+			value := args[0]
+			message := value.Get("message").String()
+
+			channel <- &decrypt_state{
+				buffer: []byte{},
+				err:    errors.New(message),
+			}
+
+			return nil
+
+		})
+
+		defer on_failure.Release()
+
+		subtle := js.Global().Get("crypto").Get("subtle")
+
+		if subtle.IsNull() || subtle.IsUndefined() {
+			err := errors.New("Error: Unsecure WebPage Context.")
+			return []byte{}, err
 		}
 
-		return nil
+		go subtle.Call("decrypt", wrapped_algorithm, wrapped_key, wrapped_buffer).Call("then", on_success).Call("catch", on_failure)
 
-	})
+		state := <-channel
 
-	defer on_success.Release()
+		return state.buffer, state.err
 
-	on_failure := js.FuncOf(func(this js.Value, args []js.Value) any {
-
-		value := args[0]
-		message := value.Get("message").String()
-
-		channel <- &decrypt_state{
-			buffer: []byte{},
-			err:    errors.New(message),
-		}
-
-		return nil
-
-	})
-
-	defer on_failure.Release()
-
-	subtle := js.Global().Get("crypto").Get("subtle")
-
-	if subtle.IsNull() || subtle.IsUndefined() {
-		err := errors.New("Error: Unsecure WebPage Context.")
-		return []byte{}, err
+	} else {
+		return []byte{}, errors.New("Error: Invalid CryptoKey")
 	}
-
-	go subtle.Call("decrypt", wrapped_algorithm, wrapped_key, wrapped_buffer).Call("then", on_success).Call("catch", on_failure)
-
-	state := <-channel
-
-	return state.buffer, state.err
 
 }
